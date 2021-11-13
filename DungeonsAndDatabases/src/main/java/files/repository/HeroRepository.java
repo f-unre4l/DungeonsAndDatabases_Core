3a92042a96032415d4457a3ff65f16c0dc1497f4
Dan <danspanni@gmail.com>
2021-11-13 20:57:03 +0100

    added basic repository

---
+++ DungeonsAndDatabases/src/main/java/files/repository/HeroRepository.java
@@ -0,0 +1,14 @@
+package files.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import files.model.Hero;
+
+public interface HeroRepository extends JpaRepository<Hero, Long> {
+    List<Hero> findByRace(String race);
+
+    List<Hero> findByHeroClassContaining(String heroClass);
+}
+
