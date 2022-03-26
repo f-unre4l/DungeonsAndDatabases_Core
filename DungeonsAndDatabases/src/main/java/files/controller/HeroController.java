a17114d694b063ba7cd7a644edeac0096229df0f
Dan <danspanni@gmail.com>
2022-03-26 02:19:39 +0100

    added logging

--- DungeonsAndDatabases/src/main/java/files/controller/HeroController.java
+++ DungeonsAndDatabases/src/main/java/files/controller/HeroController.java
@@ -10,8 +10,15 @@ import files.model.enums.HeroRace;
 import files.repository.HeroRepository;
 import files.services.CalculatorService;
 import files.services.ExternalAPIService;
+import files.services.LoggingService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -26,13 +33,21 @@ import java.util.*;
 @CrossOrigin(origins = "http://localhost:8081")
 @RestController
 @RequestMapping("/api/v1/heroes")
+@Tag(name = "hero-controller", description = "Creates and manages all heroes in the project")
 public class HeroController {
+
+    Logger logger = LoggerFactory.getLogger(HeroController.class);
 
     @Autowired
     HeroRepository heroRepository;
 
     @PostMapping("")
-    public ResponseEntity<Hero> createHero(@RequestBody HeroCreationDto heroCreationDto, @RequestParam boolean randomHitpoints) {
+    @ApiOperation(value = "\"createHero\" adds a hero to the storage/s",
+            notes = "Adds the information you provide about a new heroes to the storage")
+    public ResponseEntity<Hero> createHero(
+            @RequestBody HeroCreationDto heroCreationDto,
+            @ApiParam(value = "Set this to true if you want the hitpoints of your hero to be rolled with dice "
+                              + "instead of getting the average") @RequestParam boolean randomHitpoints) {
         try {
             Hero _hero = heroRepository.save(createHeroFromDto(heroCreationDto));
             ResponseEntity<Hero> response = new ResponseEntity<>(_hero, HttpStatus.CREATED);
@@ -44,18 +59,25 @@ public class HeroController {
             ResponseEntity<HeroStatsDto> responseStats = new RestTemplate().postForEntity(
                     "http://localhost:8079/api/v1/herostats", request, HeroStatsDto.class);
             if (! responseStats.getStatusCode().is2xxSuccessful()) {
+                LoggingService.logWarnRequest(logger, responseStats.getStatusCode());
                 return new ResponseEntity<>(null, responseStats.getStatusCode());
             }
+            LoggingService.logInfoSuccess(logger, HttpMethod.POST, response.getBody());
             return response;
         } catch (IllegalArgumentException e) {
+            LoggingService.logError(logger, e, "The race or class of the hero is not valid.");
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        } catch (NullPointerException e) {
+            LoggingService.logError(logger, e, "Some necessary parameter were missing.");
             return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
         } catch (Exception e) {
-            System.out.println(e);
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+            return handleError(e);
         }
     }
 
     @DeleteMapping("")
+    @ApiOperation(value = "\"deleteAllHeros\" deletes all heroes",
+            notes = "Empties all the storages")
     public ResponseEntity<HttpStatus> deleteAllHeros() {
         try {
             heroRepository.deleteAll();
@@ -65,59 +87,65 @@ public class HeroController {
                     HttpResponse.BodyHandlers.ofString()
             );
             if (! HttpStatus.valueOf(response.statusCode()).is2xxSuccessful()) {
+                LoggingService.logWarnRequest(logger, HttpStatus.valueOf(response.statusCode()));
                 return new ResponseEntity<>(null, HttpStatus.valueOf(response.statusCode()));
             }
+            LoggingService.logInfoSuccess(logger, HttpMethod.DELETE, "all heroes");
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            return handleError(e);
         }
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<HttpStatus> deleteHero(@PathVariable("id") UUID id) {
+    @DeleteMapping("/{uuid}")
+    @ApiOperation(value = "\"deleteHero\" deletes a specific hero",
+            notes = "Deletes specific hero from all storages")
+    public ResponseEntity<HttpStatus> deleteHero(@ApiParam(value = "UUID of the hero you want to delete") @PathVariable("uuid") UUID uuid) {
         try {
-            heroRepository.deleteById(id);
+            heroRepository.deleteById(uuid);
 
             HttpResponse<String> response = HttpClient.newHttpClient().send(
-                    HttpRequest.newBuilder().DELETE().uri(new URI("http://localhost:8079/api/v1/herostats/" + id)).build(),
+                    HttpRequest.newBuilder().DELETE().uri(new URI("http://localhost:8079/api/v1/herostats/" + uuid)).build(),
                     HttpResponse.BodyHandlers.ofString()
             );
             if (! HttpStatus.valueOf(response.statusCode()).is2xxSuccessful()) {
+                LoggingService.logWarnRequest(logger, HttpStatus.valueOf(response.statusCode()));
                 return new ResponseEntity<>(null, HttpStatus.valueOf(response.statusCode()));
             }
+            LoggingService.logInfoSuccess(logger, HttpMethod.DELETE, "hero with uuid - " + uuid);
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            return handleError(e);
         }
     }
 
     @GetMapping("")
-    public ResponseEntity<List<Hero>> getAllHeros(@RequestParam(required = false) String name) {
+    @ApiOperation(value = "\"getAllHeros\" returns all heroes",
+            notes = "Returns the basic information about all the heroes in the storage")
+    public ResponseEntity<List<Hero>> getAllHeros() {
         try {
-            List<Hero> heroes = new ArrayList<>();
-
-            if (name == null) {
-                heroes.addAll(heroRepository.findAll());
-            } else {
-                heroes.addAll(heroRepository.findByNameContaining(name));
-            }
+            List<Hero> heroes = new ArrayList<>(heroRepository.findAll());
 
             if (heroes.isEmpty()) {
+                LoggingService.logWarnRequest(logger, HttpStatus.NO_CONTENT);
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
             }
-
+            LoggingService.logInfoSuccess(logger, HttpMethod.GET, heroes);
             return new ResponseEntity<>(heroes, HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+            return handleError(e);
         }
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<HeroDto> getHeroById(@PathVariable("id") UUID id) {
-        Optional<Hero> heroData = heroRepository.findById(id);
+    @GetMapping("/{uuid}")
+    @ApiOperation(value = "\"getHeroById\" returns details about a specific hero",
+            notes = "Returns all the information about the specific hero your're looking for")
+    public ResponseEntity<HeroDto> getHeroById(@ApiParam(value = "UUID of the hero you want more information on") @PathVariable("uuid") UUID uuid) {
+        Optional<Hero> heroData = heroRepository.findById(uuid);
 
-        ResponseEntity<HeroStatsDto> responseStats = new RestTemplate().getForEntity("http://localhost:8079/api/v1/herostats/{id}", HeroStatsDto.class, id);
+        ResponseEntity<HeroStatsDto> responseStats = new RestTemplate().getForEntity("http://localhost:8079/api/v1/herostats/{uuid}", HeroStatsDto.class, uuid);
         if (! responseStats.getStatusCode().is2xxSuccessful()) {
+            LoggingService.logWarnRequest(logger, responseStats.getStatusCode());
             return new ResponseEntity<>(null, responseStats.getStatusCode());
         }
         HeroStatsDto stats = Objects.requireNonNull(responseStats.getBody());
@@ -126,10 +154,13 @@ public class HeroController {
                 heroData.get(),
                 stats,
                 heroCalculationDto)));
-        return heroDto.map(hero -> new ResponseEntity<>(hero, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        ResponseEntity<HeroDto> response = heroDto.map(hero -> new ResponseEntity<>(hero, HttpStatus.OK)).orElseGet(
+                () -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        LoggingService.logInfoSuccess(logger, HttpMethod.GET, response.getBody());
+        return response;
     }
 
-    private Hero createHeroFromDto(@RequestBody HeroCreationDto heroCreationDto) {
+    private Hero createHeroFromDto(@RequestBody HeroCreationDto heroCreationDto) throws IllegalArgumentException, NullPointerException {
         return new Hero(
                 heroCreationDto.getName(),
                 validateRace(heroCreationDto.getRace()),
@@ -137,11 +168,16 @@ public class HeroController {
         );
     }
 
-    private String validateHeroClass(String heroClass) {
+    private <T> ResponseEntity<T> handleError(Exception e) {
+        LoggingService.logError(logger, e);
+        return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private String validateHeroClass(String heroClass) throws IllegalArgumentException, NullPointerException {
         return HeroClass.valueOf(heroClass.toUpperCase()).getValidHeroClass();
     }
 
-    private String validateRace(String race) throws IllegalArgumentException {
-        return HeroRace.valueOf(race.toUpperCase(Locale.ROOT)).getValidRace();
+    private String validateRace(String race) throws IllegalArgumentException, NullPointerException {
+        return HeroRace.valueOf(race.toUpperCase(Locale.ROOT)).getName();
     }
 }
---
+++ DungeonsAndDatabases/src/main/java/files/services/LoggingService.java
@@ -0,0 +1,29 @@
+package files.services;
+
+import org.slf4j.Logger;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoggingService {
+    public static String apiDocumentation = "Please look at the API Docu for more information: http://localhost:8080/swagger-ui/index.html#/";
+
+    public static void logError(Logger logger, Exception e) {
+        logger.error("Something went wrong. " + apiDocumentation, e);
+    }
+
+    public static void logError(Logger logger, Exception e, String message) {
+        logger.error(message + " " + apiDocumentation, e);
+    }
+
+    public static void logWarnRequest(Logger logger, HttpStatus status) {
+        logger.warn("Request went wrong. StatusCode: " + status);
+    }
+
+    public static<T> void logInfoSuccess(Logger logger, HttpMethod httpMethod, T object) {
+        logger.info("successful " +httpMethod.name()+" of: " + object);
+    }
+
+}
